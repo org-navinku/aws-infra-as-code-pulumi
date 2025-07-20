b487e979c67a17337783a2a0a2bb80c13b74376d
Navin Kumar <navinkumar-git@trendintown.com>
2025-07-20 10:57:35 +0000

    TIT-6 boto3 s3 ls

--- s3/__main__.py
+++ s3/__main__.py
@@ -1,10 +1,15 @@
-"""An AWS Python Pulumi program"""
 
 import pulumi
 from pulumi_aws import s3
+import boto3
+
+s3 = boto3.client('s3')
+
+for bucket in s3.list_buckets()['Buckets']:
+    print(f'Found bucket: {bucket["Name"]}')
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+# bucket = s3.BucketV2('my-bucket')
 
 # Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# pulumi.export('bucket_name', bucket.id)
